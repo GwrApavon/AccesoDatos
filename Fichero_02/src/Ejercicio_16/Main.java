5d633ca0fe04b1922890ad0621c1c61887b71c2d
Ángel Pavón Fraile <bc.apavon.1995@iespabloserrano.com>
2022-10-05 12:40:09 +0200

    Ejercicio Agenda RandomFile Encapsulado

---
+++ Fichero_02/src/Ejercicio_16/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Ejercicio_16;
+
+/**
+ * @author alu
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fichero_02/src/RandomFile/Main.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package RandomFile;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * @author alu
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GestionaAgenda agenda = new GestionaAgenda("src//aleatorio//encapsulado//AgendaPersonas.dat");
+		
+		
+		//  guarda las personas en el fichero secuencial
+		// la primera persona estar� en la posici�n 1, la segunda en la posici�n 2
+		// y as� sucesivamente....
+		try {
+			agenda.abrir();
+			
+			// escribir las personas
+			Persona p1 = new Persona("Angela", 44, "angela@gmail.com");
+			Persona p2 = new Persona("Pedro", 22, "pedro@gmail.com");
+			Persona p3 = new Persona("Luis",33, "luis@gmail.com");
+			Persona p4 = new Persona("Ana", 24, "ana@gmail.com");
+			
+			agenda.escribir(p1,1);
+			agenda.escribir(p2,2);
+			agenda.escribir(p3,3);
+			agenda.escribir(p4,4);
+			
+			/*Persona pLeo = new Persona();
+			pLeo = agenda.leer(2);
+			System.out.println(pLeo);
+			*/
+			Persona pLeoTodo = new Persona();
+			Persona [] registro;
+			registro = pLeoTodo.
+			
+		} // COMPLETA CON LOS CATCH QUE HAGAN FALTA  
+		catch (FileNotFoundException e) {
+			System.out.println("Error, fichero no econtrado");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error, de escritura");
+			e.printStackTrace();
+		}
+		
+		// leer las personas
+		try {
+			
+			System.out.println(agenda.leer(3));
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
